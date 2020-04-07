8910c1236e6234b8f56b367347d75c778d62991f
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-04-07 12:56:19 -0400

    added nav links/routes and added landingpage component. displays landing page. no styling done yet. next work on context with login. also add sign up in landing page

---
+++ src/Contexts/UserContext.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UserContext extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserContext
